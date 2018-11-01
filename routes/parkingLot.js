171a48c04785ed8d1f53744b4b3057c066e98ad6
pofay <Pofay>
2018-11-01 16:31:58 +0800

    Jquery hookup is now complete along with UI

--- routes/parkingLot.js
+++ routes/parkingLot.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('parkingLot', { title: 'Parking Lot Example'})
 });
 
 module.exports = router;
